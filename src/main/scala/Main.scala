cca78008bf8d4a2695e88578f78a187735a72ca5
ellchow <drag0nxspirit@gmail.com>
2013-02-04 13:37:14 -0800

    slf4j example

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,8 +1,13 @@
 import scalaz._
 import Scalaz._
 
+import org.slf4j.LoggerFactory
+
 object Main{
+  def log = LoggerFactory.getLogger(getClass.getName)
+
   def main(args: Array[String]){
     "hello world!" |> println
+    log info "some info statement..."
   }
 }
